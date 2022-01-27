b536026fa99bfbebf18cec34b803912c01897fac
Gabriel Rodrigues <0040892@academico.ifmg.edu.br>
2022-01-27 19:06:08 -0300

    bettering when showing  visual id transition

--- src/components/Presentation/styles.js
+++ src/components/Presentation/styles.js
@@ -3,9 +3,9 @@ import binary from "../../assets/backgrounds/binary.jpg";
 import {colorScheme} from "../../styles/themes";
 
 export const Parallax = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   background-image: url(${binary});
   background-attachment: fixed;
@@ -14,125 +14,125 @@ export const Parallax = styled.div`
   background-size: cover;
 
   min-height: 100vh;
-	
-	.p-content{
-		display: flex;
-		flex-flow:column;	
-		justify-content: space-evenly;
-		align-items: center;
-		
-		max-width:80vw;
-		height: 80vh;
-		
+
+  .p-content {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-evenly;
+    align-items: center;
+
+    max-width: 80vw;
+    height: 80vh;
+
     z-index: 1;
-		
-		@media (min-width: 769px) {
-    	max-width:50vw;
-  	}
-		
+
+    @media (min-width: 769px) {
+      max-width: 50vw;
+    }
+
     @media (min-width: 1920px) {
-      max-width:30vw;
+      max-width: 30vw;
     }
   }
-	
-  .p-content__logo-hover-show{
-		opacity: 0;
+
+  .p-content__logo-hover-show {
+    opacity: 0;
   }
 
-  .p-content__text{
-		text-align: center;
-		font-weight: 200;
+  .p-content__text {
+    text-align: center;
+    font-weight: 200;
     font-size: 40px;
     color: white;
 
     @media (min-width: 1025px) {
       font-size: 48px;
     }
-		
+
     @media (min-width: 1920px) {
       font-size: 64px;
     }
   }
 
-  .p-content__text__name{
-    color: ${({theme})=>theme.softAccent};
+  .p-content__text__name {
+    color: ${({theme}) => theme.softAccent};
   }
 
-  .p-content__socials{
-		display: flex;
-		gap: 20px;
+  .p-content__socials {
+    display: flex;
+    gap: 20px;
 
   }
 
-  .p-content__socials__icon{
-		height: 40px;
-		width: unset;
-		
-	  border: 3px solid ${colorScheme.offWhite};
+  .p-content__socials__icon {
+    height: 40px;
+    width: unset;
+
+    border: 3px solid ${colorScheme.offWhite};
     border-radius: 100%;
     overflow: hidden;
-		
-		cursor: pointer;
-		
-		fill: ${colorScheme.offWhite};
-	  
-	  &:hover{
-		  transform: scale(1.25);
-	  }
-		
+
+    cursor: pointer;
+
+    fill: ${colorScheme.offWhite};
+
+    &:hover {
+      transform: scale(1.25);
+    }
+
     @media (min-width: 1920px) {
       height: 51px;
     }
   }
 
-  .p-content__arrow{
-		position: absolute;
-		width: 45px;
-		height: unset;
-		
-		bottom: 10px;
+  .p-content__arrow {
+    position: absolute;
+    width: 45px;
+    height: unset;
+
+    bottom: 10px;
 
     cursor: pointer;
 
     transform: scale(1);
     animation: pulse 2s infinite;
   }
-	
-	::before{
-		content: "";
-		display: flex;
-		
+
+  ::before {
+    content: "";
+    display: flex;
+
     position: absolute;
-		left: 0;
-		top: 0;
+    left: 0;
+    top: 0;
 
     min-height: 100vh;
-		width: 100%;
+    width: 100%;
 
-    background-color: rgb(0,0,0);
-		
-		opacity: 0.8;
-		z-index: 0;
-	}
+    background-color: rgb(0, 0, 0);
+
+    opacity: 0.8;
+    z-index: 0;
+  }
 `
 export const TransitionLogo = styled.div`
-		cursor: pointer;
+  cursor: pointer;
   position: relative;
-		
-			background-image: url(${({image})=>image});
-			transition: background .5s;
-		
-		&:hover{
-		  background-image: url(${({hoverImage})=>hoverImage});
-		}
-		
-		margin-left: auto;
-		margin-right: auto;
-		width: min(175px, 80vw);
-		height: min(175px, 80vw);
-		
-		@media (min-width: 1920px) {
-		  width: min(230px, 80vw);
-		  height: min(230px, 80vw);
-		}
+
+  background-image: url(${({image}) => image});
+  transition: background .5s;
+
+  &:hover {
+    background-image: url(${({hoverImage}) => hoverImage});
+  }
+
+  margin-left: auto;
+  margin-right: auto;
+  width: min(175px, 80vw);
+  height: min(175px, 80vw);
+
+  @media (min-width: 1920px) {
+    width: min(230px, 80vw);
+    height: min(230px, 80vw);
+  }
 `
